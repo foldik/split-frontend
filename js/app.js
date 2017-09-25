382e3fa74f3c5b22618a9fb4ca61fbb688250fff
foldik-learn <foldi.kristof.work@gmail.com>
2017-09-25 19:31:25 +0200

    Created expandable card

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
+var card_text_count = 0;
+
 function addActivity() {
     var activityName = document.getElementById('activityNameInput').value;
     var time = moment().format('HH:mm:ss');
-
+    var id = 'card-text-' + card_text_count;
     var card = d3.select('#activity-container')
         .append('div')
         .attr('class', 'card')
         .append('div')
-        .attr('class', 'card-body');
+        .attr('class', 'card-body')
+        .attr('data-toggle', 'collapse')
+        .attr('data-target', '#' + id)
+        .attr('aria-expanded', 'false')
+        .attr('aria-controls', id);
 
     card.append('h5')
         .attr('class', 'card-title')
@@ -15,6 +21,13 @@ function addActivity() {
     card.append('h6')
         .attr('class', 'card-subtitle mb-2 text-muted')
         .text(time + ' - Present');
+
+    card.append('p')
+        .attr('class', 'collapse')
+        .attr('id', id)
+        .text('Lorem dorsum ilires sot ipo catus mchecntus sipalis aarus gettus faternus. Sit Dolores perus.');
+
+    card_text_count = card_text_count + 1;
 
     document.getElementById('activityNameInput').value = '';
 
