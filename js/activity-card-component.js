d66eb13c2ac2aa6b5af482b7be234ebecc5f2a45
foldik-learn <foldi.kristof.work@gmail.com>
2017-09-27 21:15:39 +0200

    enhanced card to collapse when touch header

--- js/activity-card-component.js
+++ js/activity-card-component.js
@@ -2,8 +2,8 @@ Vue.component('activity-card', {
     props: ['cardId', 'activity', 'cardParent'],
     template: `
 		<div class="card">
-      <div class="card-header" role="tab" :id="'card-header-' + cardId">
-        <h5 :href="'#card-body-' + cardId" class="card-title" data-toggle="collapse" aria-expanded="false" :aria-controls="'card-body-' + cardId">{{activity.title}}</h5>
+      <div class="card-header" role="tab" :id="'card-header-' + cardId" data-toggle="collapse" :href="'#card-body-' + cardId" aria-expanded="false" :aria-controls="'card-body-' + cardId">
+        <h5 class="card-title">{{activity.title}}</h5>
         <h6 class="card-subtitle mb-2 text-muted">{{activity.timeinterval}}</h6>
       </div>
       <div :id="'card-body-' + cardId" class="collapse" role="tabpanel" :aria-labelledby="'card-body-' + cardId" :data-parent="'#' + cardParent">
