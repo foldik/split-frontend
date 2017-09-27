6d832e4e28f9487052bf6f64d98243d77d90bda3
foldik-learn <foldi.kristof.work@gmail.com>
2017-09-27 21:09:21 +0200

    enhanced editable

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ var addActivityForm = new Vue({
             activitiesContainer.activities.unshift({
                 title: activityName,
                 timeinterval: moment().format('HH:mm') + ' - Present',
-                description: 'Lorem impsumdolores sit on tus. Eta us peru kamisu sit misu lor. Put ron lori annaku puksa imprum elimbu.'
+                description: ''
             });
             this.activityName = '';
         }
--- js/editable-component.js
+++ js/editable-component.js
@@ -1,6 +1,6 @@
 Vue.component('editable', {
     template: `
-    <div ref="input" v-bind:value="value" v-on:input="updatedValue($event.target.innerHTML)" contenteditable="true"></div>
+    <div ref="input" v-bind:value="value" v-on:input="updatedValue($event.target.innerHTML)" contenteditable="true" data-text="Give a description to this activity"></div>
     `,
     props: ['value'],
     mounted: function() {
